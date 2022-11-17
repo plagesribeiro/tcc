dbf1d7b5e4b3c049271fbb2e9c71a1db05fff909
Pedro Lages <plagesribeiro@gmail.com>
2022-11-17 16:00:50 -0300

    aula 3

--- pages/api/openai/index.ts
+++ pages/api/openai/index.ts
@@ -32,7 +32,17 @@ export async function generateText(
 		delete openaiOptions.engine;
 
 		try {
-			const openAiResp = await openai.createCompletion(
+			const openAiResp = await query(
+				{
+					model: engine,
+					prompt: text,
+					n: quantity,
+					...openaiOptions
+				},
+				openAiKey
+			);
+
+			/*openai.createCompletion(
 				{
 					model: engine,
 					prompt: text,
@@ -42,12 +52,12 @@ export async function generateText(
 				{
 					timeout: 20000
 				}
-			);
+			);*/
 
 			if (openAiResp.data.choices) {
 				let i = 0;
 				await Promise.all(
-					openAiResp.data.choices.map((choice) => {
+					openAiResp.data.choices.map((choice: any) => {
 						if (choice.text) {
 							const formattedText = formatOutput(skill, choice.text);
 							completions[i] = formattedText;
@@ -91,4 +101,31 @@ export default async function handler(
 	} else {
 		response.status(404).json({ message: 'Skill not found' });
 	}
+}
+
+const DEFAULT_PARAMS = {
+	model: 'text-davinci-002',
+	temperature: 0.7,
+	max_tokens: 256,
+	top_p: 1,
+	frequency_penalty: 0,
+	presence_penalty: 0
+};
+
+export async function query(params = {}, openAiKey: string) {
+	const params_ = { ...DEFAULT_PARAMS, ...params };
+	const requestOptions = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': 'Bearer ' + String(openAiKey)
+		},
+		body: JSON.stringify(params_)
+	};
+	const response = await fetch(
+		'https://api.openai.com/v1/completions',
+		requestOptions
+	);
+	const data = await response.json();
+	return data.choices[0].text;
 }
